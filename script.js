95ab3e0c570fad276db3fadf5f0f61565c007478
Aleksandr Bobrus <112059495+CrowSiberia@users.noreply.github.com>
2025-06-02 18:50:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const body = document.querySelector('body');
+const bgHexCodeSpanElement = document.querySelector('#hex-code');
+const button = document.querySelector('#button');
 
+const darkColorsArr = [
+  "#2C3E50",
+  "#34495E",
+  "#2C2C2C",
+  "#616A6B",
+  "#4A235A",
+  "#2F4F4F",
+  "#0E4B5A",
+  "#36454F",
+  "#2C3E50",
+  "#800020",
+];
+
+function getRandomIndex() {
+    const randomIndex = Math.floor(darkColorsArr.length * Math.random());
+    console.log(randomIndex);
+    return randomIndex;
+}
+
+function changeBackgroundColor() {
+    const color = darkColorsArr[getRandomIndex()];
+    console.log(color);
+    hexCode.innerText = color;
+    body.style.backgroundColor = color;
+}
+
+button.onclick = changeBackgroundColor;
